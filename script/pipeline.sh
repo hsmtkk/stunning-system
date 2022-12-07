91b0d8f1aac38c91464095827936b5e9180548a7
hsmtkk <hsmtkk@gmail.com>
2022-12-07 11:26:17 +0900

    update shell script

--- script/pipeline.sh
+++ script/pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 gcloud dataflow flex-template run "wordcount-go-`date +%Y%m%d-%H%M%S`" \
-    --template-file-gcs-location "gs://BUCKET_NAME/samples/dataflow/templates/wordcount-go.json" \
-    --parameters output="gs://BUCKET_NAME/samples/dataflow/templates/counts.txt" \
-    --region "REGION"
+    --template-file-gcs-location "gs://my-storage-stunning-system-370901/samples/dataflow/templates/wordcount-go.json" \
+    --parameters output="gs://my-storage-stunning-system-370901/samples/dataflow/templates/counts.txt" \
+    --region us-west1
