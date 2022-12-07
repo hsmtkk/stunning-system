3f2fa40884dc12b9acf478eba0a9a25f2a00b2dd
hsmtkk <hsmtkk@gmail.com>
2022-12-07 11:33:20 +0900

    set num of workers to reduce costs

--- script/pipeline.sh
+++ script/pipeline.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 gcloud dataflow flex-template run "wordcount-go-`date +%Y%m%d-%H%M%S`" \
-    --template-file-gcs-location "gs://my-storage-stunning-system-370901/samples/dataflow/templates/wordcount-go.json" \
+    --max-workers 1 \
+    --num-workers 1 \
     --parameters output="gs://my-storage-stunning-system-370901/samples/dataflow/templates/counts.txt" \
-    --region us-west1
+    --region us-west1 \
+    --template-file-gcs-location "gs://my-storage-stunning-system-370901/samples/dataflow/templates/wordcount-go.json" \
+    --worker-machine-type e2-micro
